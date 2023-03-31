540acb7b857a77cd0f1cf6f38e14d063f0d5aab9
dannyedw <danielgaraway@gmail.com>
2023-03-31 15:41:14 +0100

    fixed navigation issue and added footer to layout

--- HealthApp/app.js
+++ HealthApp/app.js
@@ -21,7 +21,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/index', indexRouter);
+app.use('/', indexRouter);
 app.use('/login', loginRouter);
 app.use('/userDashboard', userDashboardRouter);
 app.use('/groupDashboard', groupDashboardRouter);
