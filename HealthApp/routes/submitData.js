f50f9c371c45f26db2a1aa3fbf25a88ad1a755ee
Alex Faulkner <alex.w.faulkner13@gmail.com>
2023-04-03 16:41:30 +0100

    general layout for server handling data requests

---
+++ HealthApp/routes/submitData.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var router = express.Router();
+var dataReceiver = require('./model/dataReceiver');
+
+/* GET group dashboard page. */
+router.post('/submitData', function(req, res, next) {
+  
+});
+
+module.exports = router;
