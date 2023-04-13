224b693c45fdfb9c0ba02cbcb2c796c07494a536
yonneymuslia <fatjonmuslia@hotmail.co.uk>
2023-04-13 17:46:56 +0100

    added a table in exercises and organised the rest of the javascript to dashboard javascript

--- HealthApp/public/javascripts/dashboard.js
+++ HealthApp/public/javascripts/dashboard.js
@@ -1,3 +1,48 @@
+function addExercise() {
+  overlay.style.display = "block";
+  addExerciseContainer.style.display = "block";
+  }
+  function addDiet() {
+  overlay.style.display = "block";
+  addDietContainer.style.display = "block";
+  }
+  function addGoal() {
+  overlay.style.display = "block";
+  addGoalContainer.style.display = "block";
+  }
+  //gets the div's so we can hide/unhide them
+  const overlay = document.querySelector('.overlay');
+  const addExerciseContainer = document.querySelector('#addExerciseContainer');
+  const addDietContainer = document.querySelector('#addDietContainer');
+  const addGoalContainer = document.querySelector('#addGoalContainer');
+  //links the buttons to the functions
+  document.getElementById("addExerciseButton").addEventListener("click", addExercise)
+  document.getElementById("addDietButton").addEventListener("click", addDiet)
+  document.getElementById("addGoalButton").addEventListener("click", addGoal)
+  //add a anonymas function to each exit button to hide the popup
+  document.getElementById("exitButtonExercise").addEventListener("click", function () {
+  overlay.style.display = "none";
+  addExerciseContainer.style.display = "none";
+  document.getElementById("variations").innerHTML = "";
+  })
+  document.getElementById("exitButtonDiet").addEventListener("click", function () {
+  overlay.style.display = "none";
+  addDietContainer.style.display = "none";
+  })
+  document.getElementById("exitButtonGoal").addEventListener("click", function () {
+  overlay.style.display = "none";
+  addGoalContainer.style.display = "none";
+  })
+
+  document.getElementById("exitButtonGoal").addEventListener("click", function () {
+  overlay.style.display = "none";
+  addGoalContainer.style.display = "none";
+  })
+
+
+  
+
+
 const exerciseSelect = document.getElementById("exercises");
 const variationsDiv = document.getElementById("variations");
 
