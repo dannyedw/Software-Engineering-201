6ea89391c1e604f48f67d1e12d35402c881ce5e6
yonneymuslia <fatjonmuslia@hotmail.co.uk>
2023-04-30 20:17:04 +0100

    lists all the emails wanting to be added

--- HealthApp/public/javascripts/groupdashboard.js
+++ HealthApp/public/javascripts/groupdashboard.js
@@ -10,23 +10,35 @@ const overlay = document.querySelector('.overlay');
 const addGroupContainer = document.querySelector('#addGroupContainer');
 
 
-// member emails section
-// const memberEmails = document.getElementById("memberEmails")
-// memberEmails.innerHTML = "<input type='text' id= 'memberEmail' name='members-email' placeholder='Member Email' required='' size='30px'>"
+//member emails section
+const memberEmails = document.getElementById("memberEmails")
 
-// document.getElementById("addMemberEmail").addEventListener("click",addMemberEmail);
-// emails = [];
-// function addMemberEmail(){
-//     const email = document.getElementById("memberEmail").value;
-//     emails.push(email);
-//     //memberEmails.innerHTML = "";
-//     for(mail in emails){
-//         memberEmails.innerHTML += "<br>"+emails[mail]+"";
-//     }
-//     //memberEmails.innerHTML = "<input type='text' id= 'memberEmail' name='members-email' placeholder='Member Email' required='' size='30px'><button type='button' id='addMemberEmail'>ADD</button>"
-// }
+
+document.getElementById("addMemberEmail").addEventListener("click",addMemberEmail);
+emails = [];
+function addMemberEmail(){
+    const email = document.getElementById("memberEmail").value;
+    emails.push(email);
+    memberEmails.innerHTML = "";
+    for(mail in emails){
+        memberEmails.innerHTML += "<br>"+emails[mail]+"<button id=deleteEmail type=button>remove</button>";
+    }
+    document.getElementById("memberEmail").value = "";
+}
 
 document.getElementById("exitButtonCreateGroup").addEventListener("click",function(){
     overlay.style.display = "none";
 	addGroupContainer.style.display = "none";
 })
+document.getElementById("deleteEmail").addEventListener("click",function(){
+    memberEmails.innerHTML = "";
+    var temp = emails[removeMail];
+    emails[removeMail] = emails[0];
+    emails[0] = temp;
+    emails.shift();
+    for(mail in emails){
+        memberEmails.innerHTML += "<br>"+emails[mail]+"<button id=deleteEmail type=button onclick="+deleteEmail()+">delete</button>";
+    }
+})
+
+
