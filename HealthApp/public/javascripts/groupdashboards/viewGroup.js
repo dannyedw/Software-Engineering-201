705073448ac47b745409bdbd1c039da863ab57d2
yonneymuslia <fatjonmuslia@hotmail.co.uk>
2023-05-01 16:00:28 +0100

    new viewGroup Javascipt file

---
+++ HealthApp/public/javascripts/groupdashboards/viewGroup.js
@@ -0,0 +1,17 @@
+var groups = document.getElementById("groups");
+// //form request of groups from backend
+//     let data = {
+// 		type: "group-request",
+// 		content: {
+// 		}
+// 	};
+
+// 	dataRequest(data, responseHandler);
+
+// 	function responseHandler(response) {
+// 		if (response.status != 200) {
+// 			console.log(response.content)
+// 		}for(let group of response.content){
+//             groups.innerHTML = `Group Name: `+group['groupname']+` Number of Members: `+group['membersSize']+` >  `+group['memberType']+` Number of Group Goals`+group["numberGoals"]+` `
+//     }
+// 	}
