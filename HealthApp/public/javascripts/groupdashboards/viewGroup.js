49e35365c40e2ccb1473128784d56fc0b0411e6e
yonneymuslia <fatjonmuslia@hotmail.co.uk>
2023-05-03 17:47:30 +0100

    adding some examples to the dashboard

--- HealthApp/public/javascripts/groupdashboards/viewGroup.js
+++ HealthApp/public/javascripts/groupdashboards/viewGroup.js
@@ -1,4 +1,20 @@
 var groups = document.getElementById("groups");
+//groups.addEventListener("click",func);
+function selectGroup(groupname,groupsize,memberType,numberOfGoals){
+    console.log(groupname);
+    console.log(groupsize);
+    console.log(memberType);
+    console.log(numberOfGoals)
+}
+var groupname = "name";
+var membersSize = 10;
+var memberType = "Admin";
+var numOfGoals = 5;
+groups.innerHTML = `<h1 onclick=selectGroup(`+"5" + ","+"membersSize"+","+"memberType"+","+"numOfGoals"+`);>Group Name: `+groupname+` Number of Members: `+membersSize+` >  `+memberType+` Number of Group Goals `+numOfGoals+`  </h1> <br>`
+groups.innerHTML += `<br> <h1 onclick=selectGroup(`+"4" + ","+"membersSize"+","+"memberType"+","+"numOfGoals"+`);>Group Name: `+4+` Number of Members: `+membersSize+` >  `+memberType+` Number of Group Goals `+numOfGoals+`  </h1>`
+groups.innerHTML += `<br> <h1 onclick=selectGroup(`+"3" + ","+"membersSize"+","+"memberType"+","+"numOfGoals"+`);>Group Name: `+3+` Number of Members: `+membersSize+` >  `+memberType+` Number of Group Goals `+numOfGoals+`  </h1>`
+groups.innerHTML += `<br> <h1 onclick=selectGroup(`+"2" + ","+"membersSize"+","+"memberType"+","+"numOfGoals"+`);>Group Name: `+2+` Number of Members: `+membersSize+` >  `+memberType+` Number of Group Goals `+numOfGoals+`  </h1>`
+groups.innerHTML += `<br> <h1 onclick=selectGroup(`+"1" + ","+"membersSize"+","+"memberType"+","+"numOfGoals"+`);>Group Name: `+1+` Number of Members: `+membersSize+` >  `+memberType+` Number of Group Goals `+numOfGoals+`  </h1>`
 // //form request of groups from backend
 //     let data = {
 // 		type: "group-request",
