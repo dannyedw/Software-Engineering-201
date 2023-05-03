a72d980eb8b10d4e915d7bcabaa29adc22197167
Svetli03 <xwd21hqu@uea.ac.uk>
2023-05-03 23:38:07 +0100

    Responsive pages except for dashboard and group dashboard.

--- HealthApp/public/javascripts/dashboard.js
+++ HealthApp/public/javascripts/dashboard.js
@@ -699,12 +699,13 @@ function getAndFormatCurrentDate() {
 	var year = CurrentDate.getFullYear();
 	var month = CurrentDate.getMonth() + 1;
 	var day = CurrentDate.getDate();
-	if (date < 10) {
-		day = "0" + date;
+	if (day < 10) {
+		day = "0" + day;
 	}
 	if (month < 10) {
 		month = "0" + month;
 	}
+	
 	return year + "-" + month + "-" + day;
 }
 
