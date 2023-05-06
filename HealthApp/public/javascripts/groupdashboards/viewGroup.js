a559cf47559045008cf0307f8260a784a0f25f17
yonneymuslia <fatjonmuslia@hotmail.co.uk>
2023-05-06 18:01:58 +0100

    description and leave group button

--- HealthApp/public/javascripts/groupdashboards/viewGroup.js
+++ HealthApp/public/javascripts/groupdashboards/viewGroup.js
@@ -30,7 +30,9 @@ function displayGroup(data)
             currentGroup = data.content[key];
 
             groups.innerHTML += `<div><h1 id="divsID" class='collapsible'>` + key + `:` + ` Members: `+currentGroup.members.length+` ` + ` Group Goals: `+10+`</h1>
-            <div class='content'><div class='groupDivs'id="` + key + "-info" + `"> Members<br> </div> <div class='groupDivs' id = "` + key + "-goals" + `"> Group Goals </div> </div> </div> <br>`;
+            <div class='content'><div class='groupDivs'id="` + key + "-info" + `"> Members<br> </div> <div class='groupDivs' id = "` + key + "-goals" + `"> Group Goals </div>   <br>
+            <button type="button" id="description" onclick="DisplayDescription('${currentGroup.description}');">Description</button><br>
+            <button type="button" id="LeaveGroupButton" onclick="LeavegGroup('${key}');">LeaveGroup</button> </div></div>`;
 
             var membersSection = document.getElementById(key + "-info"); 
             var groupGoalsSection = document.getElementById(key + "-goals");    
@@ -58,15 +60,11 @@ function displayGroup(data)
 function displayGroupInfo(data, divId)
 {
     container = document.getElementById(divId);
-    // members=[]
-    // for (var i = 0; i < data.members.length; i++) {
-    //     var member = data.members[i]
-    //     members.push(member)
-    // }
-    console.log(data)
+
     for (var i = 0; i < data.members.length; i++) {
         var member = data.members[i]
-        container.innerHTML += member+'<button id=somebutton type=button onclick="removeMember(\'' +member+ '\',\'' +data +'\',\'' +divId +'\')">remove</button><br>';
+      
+        container.innerHTML += member+'<button id=somebutton type=button onclick="removeMember(\'' +i+ '\',\'' +data.members +'\',\'' +divId +'\')">remove</button><br>';
     
     }
     // container.innerHTML += '<br><br><input type=text id=membername placeholder=Member Name>';
@@ -81,11 +79,13 @@ function addMember(member,data){
     console.log("click")
 }
 function removeMember(member,data,div){
+    data = data.split(',')
+  
     // removes user from server
     // data ={
     //     type:"remove-user",
     //     content:{
-    //         username:"Username"
+    //         username:"USERNAME"
     //     }
     // }
     // dataRequest(data,removeMemberHandler)
@@ -94,27 +94,51 @@ function removeMember(member,data,div){
     //         console.log(response.content)
     //     }
     // }
-    console.log(data)
-    // container = document.getElementById(div);
-    // var temp = members[member];
-    // members[member] = members[0];
-    // members[0] = temp;
-    // members.pop[0];
-
-    // for (var i = 0; i < data.members.length; i++) {
-    //     var member = data.members[i]
-    //     console.log(member)
-    //     container.innerHTML += member+'<button id=somebutton type=button onclick="removeMember(\'' +member+ '\',\'' +data +'\',\'' +div +'\',\'' +members +'\')">remove</button><br>';
+ 
+    container = document.getElementById(div);
+    if(data.length == 1){
+        data.pop[0];
+        data.shift();
+        container.innerHTML="";
+    }else{
+        const temp = data[member];
+        data[member] = data[0];
+        data[0] = temp;
+        data.pop[0];
+        
+        data.shift()
+        console.log(data)
+        container.innerHTML="";
+    for (var i = 0; i < data.length; i++) {
+        var member = data[i]
+        container.innerHTML += member+'<button id=somebutton type=button onclick="removeMember(\'' +member+ '\',\'' +data +'\',\'' +div +'\')">remove</button><br>';
     
+    }
+    }
+}
+function DisplayDescription(description){
+    console.log(description);
+}
+function LeavegGroup(groupName){
+    console.log("leaving group:"+groupName);
+     // removes user from server
+    // data ={
+    //     type:"group-delete",
+    //     content:{
+    //         groupname:groupName
+    //     }
     // }
-
-
+    // dataRequest(data,leaveGroupHandler)
+    // function leaveGroupHandler(response){
+    //     if(response.status !=200){
+    //         console.log(response.content)
+    //     }
+    // }
 }
 
 function displayGroupGoals(data, divId)
 {
     container = document.getElementById(divId);
-    //console.log(data);
     container.innerHTML = `<p>Hello goals here</p>`
 }
 
